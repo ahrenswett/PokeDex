c8032908c3c445321c02195b23a90a2b5b19cf79
Ahren Swett <ahreswett@gmail.com>
2022-07-07 08:40:05 -0700

    Setting up Navigation and package files

---
+++ app/src/main/java/com/ahrenswett/samaritanpokedex/navigation/Navigation.kt
@@ -0,0 +1,11 @@
+package com.ahrenswett.samaritanpokedex.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+
+@Composable
+fun Navigation(){
+    val navController = remeberNavController()
+    NavHost()
+
+}
